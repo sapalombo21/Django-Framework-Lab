b56ffa25aafa2006f44ecad9cb924558ee2405a2
sapalombo21 <sam.palombo.2001@gmail.com>
2022-04-25 15:06:30 -0500

    game model created and migrated

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=200)
+    summary = models.TextField(max_length=250)
+    price = models.FloatField()
+    series = models.CharField(max_length=200)
