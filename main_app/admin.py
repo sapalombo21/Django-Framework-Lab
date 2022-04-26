c3c6a18277b3c498931bc86bf4f72fbc14638252
sapalombo21 <sam.palombo.2001@gmail.com>
2022-04-26 15:13:07 -0500

    admin page now has reviews

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Game
+from .models import Game, Review
 
 admin.site.register(Game)
+admin.site.register(Review)
