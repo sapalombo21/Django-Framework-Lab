adff8ffa67f736130e47e8f8eb3830b5f81cfb4d
sapalombo21 <sam.palombo.2001@gmail.com>
2022-04-26 15:09:21 -0500

    review model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Game(models.Model):
@@ -13,3 +14,12 @@ class Game(models.Model):
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse('detail', kwargs={'game_id': self.id})
+
+class Review(models.Model):
+    date = models.DateField()
+    review = models.TextField(max_length=250)
+    rating = models.IntegerField(default=5, validators=[MaxValueValidator(5),MinValueValidator(1)])
+    game = models.ForeignKey(Game, on_delete=models.CASCADE) # game_id
+    def __str__(self):
+        return f"{self.review}: {self.rating}, {self.date}"
+
